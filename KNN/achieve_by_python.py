b8a513f564e024d0c862d84cd31ceabbb85f78e9
chaoge <chao3236@gmail.com>
2018-07-28 15:45:12 +0800

    add comment

--- KNN/achieve_by_python.py
+++ KNN/achieve_by_python.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 #_*_ coding:utf-8 _*_
 
+#通过直接对k-近邻算法的描述来构建鸢尾花数据集的模型，并利用该模型对鸢尾花类型进行预测
+
 import numpy as np
 import random
 import operator
 from init_data import load_data
 
 def data_split():
+
+    """
+    data_split函数的主要作用是将原始数据分为训练数据和测试数据，其中训练数据和测试数据的比例为2：1
+    """
 
     x_data,y_data = load_data()
     x_training = []
@@ -28,21 +34,36 @@ def data_split():
 
 def euclidean_distance(x_training,row):
 
+    """
+    euclidean_distance函数的主要功能是计算每一条测试数据和训练数据集的欧式距离
+    :param x_training: 训练数据集
+    :param row: 一条测试数据
+    :return: 表示欧式距离的矩阵
+    """
+
     dis = np.sum((row - x_training)**2,axis=1)
     dis = np.sqrt(dis)
     return dis
 
 def predict(dis,y_training,k):
 
-    dis_sort = np.argsort(dis)
-    statistics = {}
+    """
+    predict函数的主要作用是通过计算所得的欧式距离的集合，从中选取k个距离最小的数据点，统计这k个数据点中各个类别所出现的次数，出现次数最多的类别即为预测值
+    :param dis: 表示欧式距离的矩阵
+    :param y_training: 训练数据的类别
+    :param k: 选取k个距离最近的数据
+    :return: 预测值
+    """
+
+    dis_sort = np.argsort(dis)#对欧式距离集合进行排序，返回的dis_sort表示的是排序（从小到大）后的数据在原数组中的索引
+    statistics = {}#定义字典，用于统计k个数据点中各个类别的鸢尾花出现的次数
     for i in range(k):
         rank = dis_sort[i]
         if y_training[rank] in statistics:
             statistics[y_training[rank]] = statistics[y_training[rank]] + 1
         else:
             statistics[y_training[rank]] = 1
-    sort_statis = sorted(statistics.items(), key=operator.itemgetter(1), reverse=True)
+    sort_statis = sorted(statistics.items(), key=operator.itemgetter(1), reverse=True)#对statistics字典按照value进行排序（从大到小）
     y_predict = sort_statis[0][0]
     return y_predict
 
--- KNN/init_data.py
+++ KNN/init_data.py
@@ -5,25 +5,31 @@ import csv
 import numpy as np
 import os
 
+#该项目所使用的数据集为UCI数据集中鸢尾花数据集，包含150条数据，分为三类，每类50条数据，每条数据包含4个属性，可通过花萼长度，花萼宽度，花瓣长度，花瓣宽度4个属性预测鸢尾花卉属于（Setosa，Versicolour，Virginica）三个种类中的哪一类
+
 def load_data():
+
+    """
+    load_data()函数的主要作用是读取csv格式的原始数据，将原始数据分为两个部分，一个属性值的集合，一个类别的集合
+    """
 
     x = []
     y = []
-    data_filename = os.path.join('iris.data')
+    data_filename = os.path.join('iris.data')#读取当前目录下的数据集
     with open(data_filename,'r') as csvfile:
         dataset = csv.reader(csvfile)
         for row in dataset:
             if row == []: break
             else:
-                data = [float(da) for da in row[:-1]]
+                data = [float(da) for da in row[:-1]]#读取每一行数据中的属性值，并将其转化为float类型
                 x.append(data)
-                if row[-1] == 'Iris-setosa':
+                if row[-1] == 'Iris-setosa':#将鸢尾花的三个类别分别用1，2，3来标识
                     y.append(1)
                 if row[-1] == 'Iris-versicolor':
                     y.append(2)
                 if row[-1] == 'Iris-virginica':
                     y.append(3)
-    x_data = np.array(x)
+    x_data = np.array(x)#将list类型转化为numpy数组类型
     y_data = np.array(y)
     return (x_data,y_data)
 
